c5540354326025dd1e890a620755a089f5bb6917
jcat <jftcatarino@gmail.com>
2023-09-20 10:44:45 +0100

    prevent multi death by making death check mutex

--- philo_actions.c
+++ philo_actions.c
@@ -6,7 +6,7 @@
 /*   By: joaoteix <joaoteix@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 22:04:25 by joaoteix          #+#    #+#             */
-/*   Updated: 2023/09/20 10:36:57 by joaoteix         ###   ########.fr       */
+/*   Updated: 2023/09/20 10:44:13 by joaoteix         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,16 @@ int	check_dead(t_params *params, int id)
 		pthread_mutex_unlock(&params->crit_mtx);
 		return (1);
 	}
-	pthread_mutex_unlock(&params->crit_mtx);
 	if ((get_ets(params) - params->philos[id].last_meal_ets) >= params->ttd)
 	{
-		pthread_mutex_lock(&params->crit_mtx);
 		params->dead_flag = 1;
-		pthread_mutex_unlock(&params->crit_mtx);
 		pthread_mutex_lock(&params->output_mtx);
 		printf("%10d %2d died\n", get_ets(params), id);
 		pthread_mutex_unlock(&params->output_mtx);
+		pthread_mutex_unlock(&params->crit_mtx);
 		return (1);
 	}
+	pthread_mutex_unlock(&params->crit_mtx);
 	return (0);
 }
 
